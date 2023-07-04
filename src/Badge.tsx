d41320de28c1b5bfed2b3954bb1afc076fa4e9ea
Topi Saavalainen <topi.saavalainen@gmail.com>
2023-07-04 19:49:15 +0300

    Get and transform image

--- src/Badge.tsx
+++ src/Badge.tsx
@@ -1,10 +1,14 @@
 import { BaseSyntheticEvent, useState } from "react";
+import './badge.css';
 
 function Badge() {
     const [fileUrl, changeFileUrl] = useState('')
+    const [x, changeX] = useState(0)
+    const [y, changeY] = useState(0)
+    const [scale, changeScale] = useState(100)
 
-    const logChange = function (e: any) {
-        console.log(e);
+    const handleTransform = function (n: number, f: (n:number) => void) {
+        f(n);
     }
 
     const fileChange = function (e: BaseSyntheticEvent) {
@@ -19,19 +23,25 @@ function Badge() {
             <form action="">
                 <label htmlFor="x">
                     X-koordinaatti
-                    <input onChange={(e) => {logChange(e)}} type="number" name="x" id="x-input" />
+                    <input onChange={(e) => handleTransform(parseInt(e.target.value), changeX)} value={x} type="number" name="x" id="x-input" />
                 </label>
                 <label htmlFor="y">
                     Y-koordinaatti
-                    <input onChange={(e) => {logChange(e)}} type="number" name="y" id="y-input" />
+                    <input onChange={(e) => handleTransform(parseInt(e.target.value), changeY)} value={y} type="number" name="y" id="y-input" />
                 </label>
                 <label htmlFor="scale">
                     Koko
-                    <input onChange={(e) => {logChange(e)}} type="number" name="scale" id="scale-input" />
+                    <input onChange={(e) => handleTransform(parseInt(e.target.value), changeScale)} value={scale} type="number" name="scale" id="scale-input" />
                 </label>
                 <input  onChange={(e) => {fileChange(e)}} type="file" accept="image/*" name="file" id="file-input" />
             </form>
-            <img src={fileUrl} alt="" />
+            <div className="badge-container">
+                <img
+                    src={fileUrl}
+                    alt=""
+                    style={{transform: `scale(${scale}%) translate(${x / scale * 100}%,${y / scale * 100}%)`}}
+                    />
+            </div>
         </>
     )
 }
