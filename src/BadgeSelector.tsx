77a53c976431d1d25d00ba2abe4c1c3fef35108c
Topi Saavalainen <topi.saavalainen@edu.turku.fi>
2024-06-18 23:11:11 +0300

    Sync problem with axios auth header, also backend threading

--- src/BadgeSelector.tsx
+++ src/BadgeSelector.tsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from "react"
 import { ErrorResponse, ListBadgeData } from "./types/interfaces"
 import axios, { AxiosError } from "axios"
+import { useAuth } from "./provider/authProvider";
 
 type Props = { onBadgeSelect: (b: ListBadgeData) => void; }
 
 function BadgeSelector({onBadgeSelect}: Props) {
     const [badgesList, setBadgesList] = useState<ListBadgeData[]>([])
     const [error, setError] = useState<string>()
+    const {user} = useAuth()
 
     useEffect(() => {
-        axios.get<ListBadgeData[]>(import.meta.env.VITE_BACKEND + '/badges')
-        .then((res)=> {
-            setBadgesList(res.data)
-            setError(undefined)
-        })
-        .catch((err: AxiosError<ErrorResponse>) => {
-            setBadgesList([])
-            const message = err.response?.data.msg || err.message
-            setError(message)
-        })
-    }, [])
+        setTimeout(() => { // There's some kind of threading issue with pimysql:https://github.com/julien-duponchelle/python-mysql-replication/issues/138
+                axios.get<ListBadgeData[]>(import.meta.env.VITE_BACKEND + '/badges')
+                .then((res)=> {
+                    setBadgesList(res.data)
+                    setError(undefined)
+                })
+                .catch((err: AxiosError<ErrorResponse>) => {
+                    setBadgesList([])
+                    const message = err.response?.data.msg || err.message
+                    setError(message)
+                })
+        }, 500);
+    }, [user])
 
     function handleBadgeChange(e: React.ChangeEvent<HTMLSelectElement>) {
         const selectedBadge: ListBadgeData | undefined = badgesList.find((b) => b.id.toString() === e.target.value)
